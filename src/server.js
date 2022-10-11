faad3653cd00532118700dbd2b4a0ebe2719b299
gabriel <gabriel.r.lacerda@hotmail.com>
2022-10-11 13:11:39 -0300

    apperror routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,28 @@ const express = require('express')
 
 const app = express()
 
+const routes = require('./routes')
+const AppError = require('./utils/AppError')
+
+app.use(express.json())
+app.use(routes)
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
+
 const PORT = 4444
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
